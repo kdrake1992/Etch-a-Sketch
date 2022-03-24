9be93ded5aa76f6880f7959a12c6c32f8022de6b
kdrake1992 <kdrake1992@outlook.com>
2022-03-24 12:23:41 +0900

    Reset and resizing options have been created

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
 "use strict"
 
+let reset = document.getElementById("reset");
 const rightside = document.querySelector(".rightside");
-rightside.style.gridTemplateColumns = 'repeat(16, 1fr)';
-rightside.style.gridTemplateRows = 'repeat(16, 1fr)';
 
-for(let i = 1; i <= 16*16; i++) {
-    let square = document.createElement('div');
-    square.addEventListener('mouseover', event => {
-        square.style.backgroundColor = 'black';
-    })
-    rightside.insertAdjacentElement("beforeend", square)
+let playGame = function(size) {
+    rightside.style.gridTemplateColumns = 'repeat(' + size + ', 1fr)';
+    rightside.style.gridTemplateRows = 'repeat(' + size + ', 1fr)';
+    
+    for(let i = 1; i <= size*size; i++) {
+        let square = document.createElement('div');
+        square.addEventListener('mouseover', event => {
+            square.style.backgroundColor = 'black';
+        })
+        rightside.insertAdjacentElement("beforeend", square)
+    }
 }
+
+playGame(16);
+
+reset.addEventListener("click", function() {
+    while(rightside.firstChild) {
+        rightside.removeChild(rightside.firstChild);
+    }
+    let newSize = prompt("Create a new board size between 2-100.")
+    if(newSize >= 2 && newSize <= 100) {
+        playGame(newSize);
+    }
+    else {
+        alert("Incorrect size. Default size of 16 will show.")
+        playGame(16.
+    }
+});
